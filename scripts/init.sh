38d619b7074a3d21ab1f5968c803357ad5ebc9fd
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-02-19 14:21:49 +0100

    refactor(init.sh): remove GCP APIs activation echo statements

--- scripts/init.sh
+++ scripts/init.sh
@@ -23,12 +23,6 @@ echo "******"
 echo "ProjectID value: $PROJECT_ID"
 echo "Backend terraform bucket: $TERRAFORM_BUCKET"
 echo "******"
-echo "GCP basic APIs activation: "
-echo "serviceusage.googleapis.com, "
-echo "cloudresourcemanager.googleapis.com, "
-echo "cloudbuild.googleapis.com, "
-echo "artifactregistry.googleapis.com"
-echo "******"
 
 # Create the GCS bucket if it doesn't exist and enable versioning
 gsutil ls -b -p $PROJECT_ID gs://$TERRAFORM_BUCKET || gsutil mb -l eu -p $PROJECT_ID gs://$TERRAFORM_BUCKET || gsutil versioning set on gs://$TERRAFORM_BUCKET
