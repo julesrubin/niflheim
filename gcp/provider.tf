2bc35840ad302be603e7a83081cf411e6b0136db
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-02-19 14:17:12 +0100

    feat(provider): reference region variable in GCP provider configuration

--- gcp/provider.tf
+++ gcp/provider.tf
@@ -1,4 +1,4 @@
 provider "google" {
   project = var.project_id # Reference project ID variable
-  region  = "us-central1"  # Define default region
+  region  = var.region     # Reference region variable
 }
