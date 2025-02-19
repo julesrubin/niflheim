c1465b4fb5029e5dc1aca89619717e46c7a886ff
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-02-19 16:58:36 +0100

    refactor(storage): remove temporary storage bucket resources from GCP

--- gcp/portfolio/storage.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "temporary_bucket" {
-  name          = "${var.project_id}-temporary-bucket-${var.context}"
-  project       = var.project_id
-  location      = var.region
-  force_destroy = true
-}
