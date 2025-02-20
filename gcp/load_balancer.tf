88b555fea4ce5e3275443c68f234556d465bc7cf
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-02-21 00:51:05 +0100

    feat(gcp): update load balancer configuration to include URL redirection and path matching

--- gcp/load_balancer.tf
+++ gcp/load_balancer.tf
@@ -1,32 +1,31 @@
-# # Define the URL map to route requests to the correct backend service
-# resource "google_compute_url_map" "default" {
-#   name = "main-url-map"
-
-#   # Default service routing to the portfolio backend
-#   default_service = module.portfolio_backend.backend_service_self_link
-# }
-
-# # Define the target HTTPS proxy, which uses the URL map
-# resource "google_compute_target_https_proxy" "default" {
-#   name             = "main-https-proxy"
-#   url_map          = google_compute_url_map.default.self_link
-#   ssl_certificates = [google_compute_managed_ssl_certificate.default.self_link]
-# }
-
-# resource "google_compute_global_forwarding_rule" "https" {
-#   name                  = "https-forwarding-rule"
-#   target                = google_compute_target_https_proxy.default.self_link
-#   load_balancing_scheme = "EXTERNAL_MANAGED"
-#   port_range            = "80"
-#   ip_protocol           = "TCP"
-#   ip_address            = google_compute_global_address.default_static_ip.address
-# }
-
 resource "google_compute_url_map" "default" {
   name = "main-url-map"
 
-  default_service = module.portfolio_backend.backend_service_id
+  # Redirect root to /portfolio
+  default_url_redirect {
+    path_redirect          = "/portfolio"
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
+    strip_query            = true
+  }
+
+  host_rule {
+    hosts        = ["julesrubin.com"]
+    path_matcher = "portfolio"
+  }
+
+  path_matcher {
+    name            = "portfolio"
+    default_service = module.portfolio_backend.backend_service_id
+
+    path_rule {
+      paths   = ["/portfolio/*"]
+      service = module.portfolio_backend.backend_service_id
+    }
+  }
+
+  # You can add more services here, like /api or /blog, with similar path matchers.
 }
+
 
 resource "google_compute_target_https_proxy" "default" {
   name = "main-https-proxy"
