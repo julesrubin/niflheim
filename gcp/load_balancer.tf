f568d660ba582a0d3184f0f4eeacd4d70293975f
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-03-03 15:42:22 +0100

    feat(gcp): update load balancer configuration to serve portfolio as default service

--- gcp/load_balancer.tf
+++ gcp/load_balancer.tf
@@ -1,12 +1,8 @@
 resource "google_compute_url_map" "default" {
   name = "main-url-map"
 
-  # Redirect root to /portfolio
-  default_url_redirect {
-    path_redirect          = "/portfolio"
-    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
-    strip_query            = true
-  }
+  # Serve the portfolio as the default service (handles "/")
+  default_service = module.portfolio_backend.backend_service_id
 
   host_rule {
     hosts        = ["julesrubin.com"]
@@ -16,16 +12,8 @@ resource "google_compute_url_map" "default" {
   path_matcher {
     name            = "portfolio"
     default_service = module.portfolio_backend.backend_service_id
-
-    path_rule {
-      paths   = ["/portfolio/*"]
-      service = module.portfolio_backend.backend_service_id
-    }
   }
-
-  # You can add more services here, like /api or /blog, with similar path matchers.
 }
-
 
 resource "google_compute_target_https_proxy" "default" {
   name = "main-https-proxy"
