a0a2b6d9eff0169764e5f918e018a92b32bceaa2
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-07-23 20:20:27 +0200

    feat(gcp): add repository connection configuration for Cloud Build

---
+++ gcp/repo_connection.tf
@@ -0,0 +1,7 @@
+resource "google_cloudbuildv2_repository" "github_repo" {
+  project           = var.project_id
+  location          = var.region
+  name              = var.repository_name
+  parent_connection = "projects/${var.project_id}/locations/${var.region}/connections/github_connection" # TODO: Update README to explain the connection process
+  remote_uri        = "https://github.com/${var.owner}/${var.repository_name}.git"
+}
