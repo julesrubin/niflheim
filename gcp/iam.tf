e38983e84233b89403e4bc8e06bde34a3fd3f3a5
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-07-23 22:35:23 +0200

    feat(gcp): refactor IAM roles for Cloud Build service account and streamline access management

--- gcp/iam.tf
+++ gcp/iam.tf
@@ -1,19 +1,59 @@
+locals {
+  cloud_build_sa_email = "serviceAccount:${google_service_account.service_account["cloud-build-deploy"].email}"
+
+  cloud_build_project_roles = {
+    "run-deployer-access" = {
+      role        = "roles/run.admin"
+      description = "Grant Cloud Build SA permission to deploy to Cloud Run"
+    }
+    "iam-sa-user-access" = {
+      role        = "roles/iam.serviceAccountUser"
+      description = "Needed to act as other SAs during deployment"
+    }
+    "logging-writer-access" = {
+      role        = "roles/logging.logWriter"
+      description = "Grant Cloud Build SA the right to write logs"
+    }
+    "secret-manager-access" = {
+      role        = "roles/secretmanager.secretAccessor"
+      description = "Grant Cloud Build SA access to Secret Manager"
+    }
+    "storage-admin-access" = {
+      role        = "roles/storage.admin"
+      description = "Grant Cloud Build SA storage admin role (for GCS bucket IAM policy access)"
+    }
+    "secretmanager-admin-access" = {
+      role        = "roles/secretmanager.admin"
+      description = "Grant Cloud Build SA Secret Manager admin role"
+    }
+    "cloudbuild-build-access" = {
+      role        = "roles/cloudbuild.builds.builder"
+      description = "Grant Cloud Build SA permission to run builds"
+    }
+    "cloudbuild-connection-access" = {
+      role        = "roles/cloudbuild.connectionAdmin"
+      description = "Grant Cloud Build SA permission to view connections"
+    }
+    "project-iam-admin-access" = {
+      role        = "roles/resourcemanager.projectIamAdmin"
+      description = "Grant Cloud Build SA Project IAM Admin role (to manage project-level IAM policies)"
+    }
+
+  }
+}
+
 # Grant Cloud Build SA access to the Terraform state bucket
 resource "google_storage_bucket_iam_member" "tf-state-access" {
   bucket = "${var.project_id}-${var.repository_name}-tfstate"
   role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${google_service_account.service_account["cloud-build-deploy"].email}"
+  member = local.cloud_build_sa_email
 }
 
-# Grant Cloud Build SA permission to deploy to Cloud Run
-resource "google_project_iam_member" "run-deployer-access" {
-  project = var.project_id
-  role    = "roles/run.admin" # Scoped down from a broad viewer/admin
-  member  = "serviceAccount:${google_service_account.service_account["cloud-build-deploy"].email}"
-}
+# Grant various project-level roles to the Cloud Build SA
+resource "google_project_iam_member" "cloud_build_project_roles" {
+  for_each = local.cloud_build_project_roles
 
-resource "google_project_iam_member" "iam-sa-user-access" {
   project = var.project_id
-  role    = "roles/iam.serviceAccountUser" # Needed to act as other SAs during deployment
-  member  = "serviceAccount:${google_service_account.service_account["cloud-build-deploy"].email}"
+  role    = each.value.role
+  member  = local.cloud_build_sa_email
 }
