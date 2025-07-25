4c4b0c2308d4a600cf0d167e5726f9071ad9fb4e
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-07-25 15:07:32 +0200

    feat(gcp): add Terraform configuration and variables for proxy service

---
+++ gcp/services/proxy/run.tf
@@ -0,0 +1,26 @@
+
+locals {
+  proxied_cloud_run_services = [
+    "portfolio",
+  ]
+}
+
+module "proxy_service" {
+  source    = "../../modules/cloud_run"
+  name      = "proxy-service"
+  location  = var.region
+  image_url = "${var.region}-docker.pkg.dev/${var.project_id}/${var.project_id}-gcr-${var.repository_name}/proxy:${var.image_tag_suffix}"
+  is_public = true
+  env_vars = {
+    for v in local.proxied_cloud_run_services :
+    "${upper(v)}_URL" => "https://${v}-${var.project_number}-${var.region}.run.app"
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "allow_all_users_to_invoke_proxy" {
+  project  = module.proxy_service.project
+  location = module.proxy_service.location
+  name     = module.proxy_service.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
