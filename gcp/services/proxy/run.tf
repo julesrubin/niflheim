0fcad821f5f5850b02d94f4a3477e0853f0c49bc
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-07-25 15:35:43 +0200

    fix(run.tf): correct environment variable naming for proxied services

--- gcp/services/proxy/run.tf
+++ gcp/services/proxy/run.tf
@@ -13,7 +13,7 @@ module "proxy_service" {
   is_public = true
   env_vars = {
     for v in local.proxied_cloud_run_services :
-    "${upper(v)}_URL" => "https://${v}-${var.project_number}.${var.region}.run.app"
+    "${upper(v)}_SERVICE_URL" => "https://${v}-${var.project_number}.${var.region}.run.app"
   }
 }
 
@@ -23,4 +23,8 @@ resource "google_cloud_run_v2_service_iam_member" "allow_all_users_to_invoke_pro
   name     = module.proxy_service.name
   role     = "roles/run.invoker"
   member   = "allUsers"
+
+  depends_on = [
+    module.proxy_service
+  ]
 }
