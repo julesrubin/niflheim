7918901713ae7a660da6af7794ff16a33542ab8f
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-07-28 10:48:02 +0200

    fix(api): remove trailing slash from items endpoint

--- backend/api/src/api/routers/items.py
+++ backend/api/src/api/routers/items.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/items/")
+@router.get("/items")
 async def read_items():
     return [{"name": "Item Foo"}, {"name": "Item Bar"}]
