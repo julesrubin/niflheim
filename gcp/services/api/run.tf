3f55b38528c97f8087868d622c95308ac1790596
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-07-28 10:24:38 +0200

    fix(api): changed cloud run listening port from 8080 to 8000

--- gcp/services/api/run.tf
+++ gcp/services/api/run.tf
@@ -1,19 +1,21 @@
 locals {
   cloud_run_services = {
     "api_service" = {
-      name      = "api"
-      is_public = true
+      name           = "api"
+      is_public      = true
+      container_port = 8000
     }
   }
 }
 
 module "cloud_run_services" {
-  source    = "../../modules/cloud_run"
-  for_each  = local.cloud_run_services
-  name      = each.value.name
-  location  = var.region
-  image_url = "${var.region}-docker.pkg.dev/${var.project_id}/${var.project_id}-gcr-${var.repository_name}/${each.value.name}:${var.image_tag_suffix}"
-  is_public = each.value.is_public
+  source         = "../../modules/cloud_run"
+  for_each       = local.cloud_run_services
+  name           = each.value.name
+  location       = var.region
+  image_url      = "${var.region}-docker.pkg.dev/${var.project_id}/${var.project_id}-gcr-${var.repository_name}/${each.value.name}:${var.image_tag_suffix}"
+  is_public      = each.value.is_public
+  container_port = each.value.container_port
 }
 
 resource "google_cloud_run_v2_service_iam_member" "cloud_run_services_invoker" {
