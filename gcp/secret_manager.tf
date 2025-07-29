903847a132fa40ac3f2ccba220eef34edc954c7a
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-07-29 15:29:05 +0200

    fix(api): remove unused Google Secret Manager configuration for GitHub token

--- gcp/secret_manager.tf
+++
@@ -1,9 +0,0 @@
-resource "google_secret_manager_secret" "github_token" {
-  project   = var.project_id
-  secret_id = "github-token"
-
-  replication {
-    auto {}
-  }
-}
-# TODO: Update README to explain how to set the secret value
