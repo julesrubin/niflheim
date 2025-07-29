02e021c3d58c3d5c253c39ff6ccc9e326355612e
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-07-29 19:14:28 +0200

    fix(proxy): add service account configuration for Cloud Run service

--- gcp/services/proxy/run.tf
+++ gcp/services/proxy/run.tf
@@ -6,12 +6,17 @@ locals {
   ]
 }
 
+data "google_service_account" "proxy_invoker" {
+  account_id = "proxy-invoker-${var.repository_name}"
+}
+
 module "proxy_service" {
-  source    = "../../modules/cloud_run"
-  name      = "proxy-service"
-  location  = var.region
-  image_url = "${var.region}-docker.pkg.dev/${var.project_id}/${var.project_id}-gcr-${var.repository_name}/proxy:${var.image_tag_suffix}"
-  is_public = true
+  source          = "../../modules/cloud_run"
+  name            = "proxy-service"
+  location        = var.region
+  image_url       = "${var.region}-docker.pkg.dev/${var.project_id}/${var.project_id}-gcr-${var.repository_name}/proxy:${var.image_tag_suffix}"
+  is_public       = true
+  service_account = data.google_service_account.proxy_invoker.email
   env_vars = {
     for v in local.proxied_cloud_run_services :
     "${upper(v)}_SERVICE_URL" => "https://${v}-${var.project_number}.${var.region}.run.app"
